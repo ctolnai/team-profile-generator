0b675ef035ab5640bfca9fe0ca0b945e031559a4
Chris Tolnai <christolnai@gmail.com>
2021-09-08 21:29:21 -0700

    working on index.js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter the name of the Employee:',
+        name: 'name',
+    }
+
+];
+
+function initialize() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {let log = "teamprofile.html";
+        
+         fs.writeFile(log, (response), (err) =>
+         err ? console.log(err) : console.log('success')
+         );
+
+});
+}
+
+initialize()
+
+module.exports = initialize;
